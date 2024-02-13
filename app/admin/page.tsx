570f9a1b492614d7a5ca01c7aa65460fa1efe79d
Araz <arazabedi@hotmail.com>
2024-02-13 14:42:50 +0000

    fixing hosting

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -15,7 +15,7 @@ import {
 	TableHead,
 	TableHeader,
 	TableRow,
-} from "components/ui/table"
+} from "@components/ui/table"
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
 
